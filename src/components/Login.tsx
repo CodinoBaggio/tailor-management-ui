66b5a82882d12662c3bf1e8af99d36063063f166
higashi <k-higashi@nihongas.co.jp>
2023-12-29 15:53:18 +0900

    2023.12.29：おー

---
+++ src/components/Login.tsx
@@ -0,0 +1,10 @@
+import { Container } from '@mui/material'
+import React from 'react'
+
+export const Login = () => {
+  return (
+    <Container maxWidth="sm">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </Container>
+  )
+}
