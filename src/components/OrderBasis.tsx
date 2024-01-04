684473ee4e3678446907938e2d3541b1a1f8623a
higashi <k-higashi@nihongas.co.jp>
2024-01-04 09:24:38 +0900

    更新：コードフォーマット

--- src/components/OrderBasis.tsx
+++ src/components/OrderBasis.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import { TextField } from '@mui/material';
 import { useFormContext } from 'react-hook-form';
 
@@ -11,13 +11,17 @@ export const OrderBasis = () => {
   const [value, setValue] = React.useState({});
 
   useEffect(() => {
-    setValue(getValues('orderBasis_orderBasisId'))
+    setValue(getValues('orderBasis_orderBasisId'));
   }, []);
 
   return (
     <>
       <div>OrderBasis</div>
-      <TextField label="発注状況" defaultValue={value} {...register('orderBasis_orderBasisId')} />
+      <TextField
+        label="発注状況"
+        defaultValue={value}
+        {...register('orderBasis_orderBasisId')}
+      />
       <div>
         {errors.orderBasis_orderBasisId
           ? errors.orderBasis_orderBasisId.message?.toString()
