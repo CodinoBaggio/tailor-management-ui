808578b1dc69163e2d9a18168e22598568bad8c0
CodinoBaggio <32092103+CodinoBaggio@users.noreply.github.com>
2024-02-07 17:58:14 +0900

    Add @mui/x-data-grid and @mui/x-data-grid-generator dependencies

--- src/features/admin/components/CustomerMaintenace.tsx
+++ src/features/admin/components/CustomerMaintenace.tsx
@@ -1,12 +1,37 @@
-import { Box } from '@mui/material';
 import React from 'react';
+import {
+  Box,
+  Checkbox,
+  FormControlLabel,
+  TextField,
+} from '@mui/material';
+import BookmarkAddIcon from '@mui/icons-material/BookmarkAdd';
+import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
 
 export const CustomerMaintenace = () => {
+  const [checked, setChecked] = React.useState(false);
+
+  const handleOwn = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setChecked(event.target.checked);
+  };
+
   return (
     <>
-      <Box className='flex'>
-        <Box className='bg-blue-50 w-48'>side</Box>
-        <Box>main</Box>
+      <Box>
+        <FormControlLabel
+          control={
+            <Checkbox
+              onChange={handleOwn}
+              value={checked}
+              icon={<BookmarkBorderIcon />}
+              checkedIcon={<BookmarkAddIcon />}
+            />
+          }
+          label="自社"
+        />
+      </Box>
+      <Box>
+        <TextField placeholder="郵便番号" type='number' />
       </Box>
     </>
   );
